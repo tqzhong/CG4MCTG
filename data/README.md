1e1dbeda7b282a44aa29aea9b8c11f35a7f029ed
R1047 <57308663+R1047@users.noreply.github.com>
2024-01-30 11:07:59 +0800

    Add files via upload
    dataset usage

---
+++ data/README.md
@@ -0,0 +1,78 @@
+## Datasets of CompMCTG Benchmark
+
+Please click [here](https://drive.google.com/file/d/1qUI7u2F9gYweinAtz3bGB7B7hpkSAOby/view?usp=sharing) to download the data.
+
+```shell
+mv data_compmctg.zip ./data
+cd ./data
+unzip data_compmctg.zip
+```
+
+### 1. Contents
+
+```
+├── Amazon
+│   ├── cls.jsonl
+│   ├── gen.jsonl
+│   └── unseen.jsonl
+├── Fyelp
+│   ├── cls.jsonl
+│   ├── gen.jsonl
+│   ├── unseen.jsonl
+├── Mixture
+│   ├── cls.jsonl
+│   ├── gen.jsonl
+│   ├── unseen.jsonl
+├── Yelp
+│   ├── cls.jsonl
+│   ├── gen.jsonl
+│   ├── unseen.jsonl
+```
+
+In this path, there are four folders named "Amazon", "Fyelp", "Mixture", and "Yelp". Each folder contains three jsonl files: "cls.jsonl", which is used for training the classifier; "gen.jsonl", which is used for training the generative model; and "unseen.jsonl", which includes all compositional attribute combinations under all modes of this dataset.
+
+### 2. Datasets Information
+
+#### 2.1 FYelp
+4 attributes: "sentiment", "gender", "cuisine", "tense"  
+		"sentiment"$\in${"Positive","Negative"}  
+		"gender"$\in${"Male","Female"}  
+		"cuisine"$\in${"Asian","American","Mexican","Bar","dessert"}  
+		"tense"$\in${"Present","Past"}  
+
+attribute combinations: 40
+		classifier data number: 40000, 1000 per one combination
+		generation data number: 70000, 1750 per one combination
+
+#### 2.2 Amazon
+
+2 attributes: "sentiment", "topic" 
+		"sentiment"$\in${"Positive","Negative"}  
+		"topic"$\in${"Books", "Clothing", "Music", "Electronics", "Movies", "Sports"}
+
+attribute combinations: 12
+		classifier data number: 180000, 15000 per one combination
+		generation data number: 120000, 10000 per one combination
+
+#### 2.3 Yelp
+3 attributes: "sentiment", "pronoun", "tense" 
+		"sentiment"$\in${"Positive","Negative"}  
+		"pronoun"$\in${"plural","singular"}
+		"tense"$\in${"Present","Past"}
+
+attribute combinations: 8
+		classifier data number: 24000, 3000 per one combination
+		generation data number: 24000, 3000 per one combination
+
+#### 2.4 Mixture(IMDB, OpeNER and Sentube)
+2 attributes: "sentiment", "topic_cged" 
+		"sentiment"$\in${"Positive","Negative"}  
+		"topic_cged"$\in${"movies", "opener", "tablets", "auto"}
+
+attribute combinations: 8
+		classifier data number: 4264, 533 per one combination
+		generation data number: 4800, 600 per one combination
+
+### 3. Usage about the datasets
+
+please check ./data/utils.py to get the usage of the datasets
--- data/utils.py
+++ data/utils.py
@@ -1,1 +1,77 @@
+import json
+import argparse
 
+def check_train(datum:dict, unseen_combs:list) -> bool:
+    '''
+    return True: datum is supposed to be in train_set;
+    otherwise: datum is supposed to be hold out;
+    '''
+    for i in range(len(unseen_combs)):
+        flag = True
+        for key in unseen_combs[i].keys():
+            if datum[key] != unseen_combs[i][key]:
+                flag = False
+        if flag == False: # at least one attribute different
+            pass
+        else: # in this combination, all the attributes match
+            return False
+    return True
+
+def get_data_by_unseen_combs(dataset_path:str, unseen_combs:list) -> list:
+    '''
+    dataset_path: contains all data of this dataset
+    unseen_combs: contains one or more attribute combinations 
+    The functions's purpose is to filter out all data from dataset_path that contains attribute combinations present in unseen_combs.
+    '''
+    f = open(dataset_path, 'r')
+    all_data = list()
+    for item in f.readlines():
+        all_data.append(json.loads(item))
+
+    data_train = list()
+    for datum in all_data:
+        if check_train(datum, unseen_combs) == True:
+            data_train.append(datum)
+    
+    return data_train
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dataset_path", default="./data/Yelp/gen.jsonl", type=str)
+    parser.add_argument("--unseen_combs_path", default="./data/unseen.jsonl", type=str)
+    parser.add_argument("--mode", default=None, type=str, choices=['Hold-Out', 'ACD', 'Few-Shot', 'Original'])
+    parser.add_argument("--idx", default=None, type=int)
+    args = parser.parse_args()
+
+    
+    '''
+    For Yelp, it contains 8 Hold-Out modes, 10 ACD modes, and 8 Few-Shot modes
+    We can create a train_set with specific mode name and its idx
+    For example, we choose mode ACD and idx = 1, then we can get train_set as follows
+    '''
+
+    args.mode = 'ACD'
+    args.idx = 1
+
+    unseen_combs_dict = {}
+    f = open(args.unseen_combs_path, 'r')
+    for item in f.readlines():
+        dic = json.loads(item)
+        unseen_combs = dic['unseen_combs']
+        idx = dic['idx']
+        mode = dic['mode']
+        if mode not in list(unseen_combs_dict.keys()):
+            unseen_combs_dict[mode] = list()
+            unseen_combs_dict[mode].append((unseen_combs, mode, idx))
+        else:
+            unseen_combs_dict[mode].append((unseen_combs, mode, idx))
+    f.close()
+
+    assert args.mode in list(unseen_combs_dict.keys())
+    assert args.idx < len(unseen_combs_dict[args.mode])
+    unseen_combs = unseen_combs_dict[args.mode][args.idx][0]
+
+    train_set = get_data_by_unseen_combs(dataset_path=args.dataset_path, unseen_combs=unseen_combs)
+
+    mode_name = unseen_combs_dict[args.mode][args.idx][1] + unseen_combs_dict[args.mode][args.idx][2]
