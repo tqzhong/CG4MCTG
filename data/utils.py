ce1a8f80fd3ef6ab7a067a894d1b5193d9c59f62
R1047 <57308663+R1047@users.noreply.github.com>
2024-01-31 19:11:09 +0800

    dataset
    dataset in CompMCTG

--- data/utils.py
+++ data/utils.py
@@ -25,21 +25,53 @@ def get_data_by_unseen_combs(dataset_path:str, unseen_combs:list) -> list:
     '''
     f = open(dataset_path, 'r')
     all_data = list()
+    all_combs = list()
     for item in f.readlines():
-        all_data.append(json.loads(item))
+        dic = json.loads(item)
+        all_data.append(dic)
+        comb = dict()
+        for key in dic.keys():
+            if key != 'review':
+                comb[key] = dic[key]
+        if comb not in all_combs:
+            all_combs.append(comb)
+    all_combs = list(all_combs)
 
     data_train = list()
     for datum in all_data:
         if check_train(datum, unseen_combs) == True:
             data_train.append(datum)
     
-    return data_train
+    return data_train, all_combs
+
+def get_train_dataset(dataset_path:str, unseen_combs_path:str, mode:str, idx:int) -> list:
+    unseen_combs_dict = {}
+    f = open(unseen_combs_path, 'r')
+    for item in f.readlines():
+        dic = json.loads(item)
+        unseen_combs = dic['unseen_combs']
+        _idx = dic['idx']
+        _mode = dic['mode']
+        if _mode not in list(unseen_combs_dict.keys()):
+            unseen_combs_dict[_mode] = list()
+            unseen_combs_dict[_mode].append((unseen_combs, _mode, _idx))
+        else:
+            unseen_combs_dict[_mode].append((unseen_combs, _mode, _idx))
+    f.close()
+    assert mode in list(unseen_combs_dict.keys())
+    assert idx < len(unseen_combs_dict[mode])
+
+    unseen_combs = unseen_combs_dict[mode][idx][0]
+    train_dataset, all_combs = get_data_by_unseen_combs(dataset_path=dataset_path, unseen_combs=unseen_combs)
+    mode_name = unseen_combs_dict[mode][idx][1] + unseen_combs_dict[mode][idx][2]
+
+    return train_dataset, mode_name, all_combs, unseen_combs
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--dataset_path", default="./data/Yelp/gen.jsonl", type=str)
-    parser.add_argument("--unseen_combs_path", default="./data/unseen.jsonl", type=str)
+    parser.add_argument("--dataset_path", default="./Yelp/gen.jsonl", type=str)
+    parser.add_argument("--unseen_combs_path", default="./Yelp/unseen.jsonl", type=str)
     parser.add_argument("--mode", default=None, type=str, choices=['Hold-Out', 'ACD', 'Few-Shot', 'Original'])
     parser.add_argument("--idx", default=None, type=int)
     args = parser.parse_args()
@@ -54,24 +86,4 @@ if __name__ == "__main__":
     args.mode = 'ACD'
     args.idx = 1
 
-    unseen_combs_dict = {}
-    f = open(args.unseen_combs_path, 'r')
-    for item in f.readlines():
-        dic = json.loads(item)
-        unseen_combs = dic['unseen_combs']
-        idx = dic['idx']
-        mode = dic['mode']
-        if mode not in list(unseen_combs_dict.keys()):
-            unseen_combs_dict[mode] = list()
-            unseen_combs_dict[mode].append((unseen_combs, mode, idx))
-        else:
-            unseen_combs_dict[mode].append((unseen_combs, mode, idx))
-    f.close()
-
-    assert args.mode in list(unseen_combs_dict.keys())
-    assert args.idx < len(unseen_combs_dict[args.mode])
-    unseen_combs = unseen_combs_dict[args.mode][args.idx][0]
-
-    train_set = get_data_by_unseen_combs(dataset_path=args.dataset_path, unseen_combs=unseen_combs)
-
-    mode_name = unseen_combs_dict[args.mode][args.idx][1] + unseen_combs_dict[args.mode][args.idx][2]
+    train_dataset, mode_name, all_combs, unseen_combs = get_train_dataset(dataset_path=args.dataset_path, unseen_combs_path=args.unseen_combs_path, mode=args.mode, idx=args.idx)
