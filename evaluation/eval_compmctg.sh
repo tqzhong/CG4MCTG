fbb0faebf55d34bf28cf3c3d838f42329319ff07
R1047 <57308663+R1047@users.noreply.github.com>
2024-02-01 01:01:18 +0800

    Add files via upload

--- evaluation/eval_compmctg.sh
+++ evaluation/eval_compmctg.sh
@@ -1,1 +1,6 @@
 
+dataset_path=/home/zhongtq/cg4ctg/test_data/ctrl/ctrl_Fyelp-v3/ctrl_fewshot=1-max_seen.jsonl
+dataset=Fyelp
+device_num=3
+
+python ./scripts/eval_compmctg.py --dataset_path $dataset_path --dataset $dataset --device_num $device_num
