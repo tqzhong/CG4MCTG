e20bf5b2c9407f84a056eff6f4afb186b9e98cfb
R1047 <57308663+R1047@users.noreply.github.com>
2024-02-01 11:03:58 +0800

    Update eval_compmctg.sh

--- evaluation/eval_compmctg.sh
+++ evaluation/eval_compmctg.sh
@@ -1,6 +1,6 @@
 
-dataset_path=/home/zhongtq/cg4ctg/test_data/ctrl/ctrl_Fyelp-v3/ctrl_fewshot=1-max_seen.jsonl
+dataset_path=./test_cases/cases_Fyelp.jsonl
 dataset=Fyelp
-device_num=3
+device_num=0
 
 python ./scripts/eval_compmctg.py --dataset_path $dataset_path --dataset $dataset --device_num $device_num
